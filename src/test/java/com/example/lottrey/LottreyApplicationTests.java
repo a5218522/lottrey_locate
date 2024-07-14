1c5ae61fd75149131f73da108510921bf3a6e00d
chy <2632797109@qq.com>
2024-07-14 13:22:40 +0800

    测试Jwt

--- src/test/java/com/example/lottrey/LottreyApplicationTests.java
+++ src/test/java/com/example/lottrey/LottreyApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.lottrey;
 
+import lottery.utils.JwtHelper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
@@ -11,9 +13,23 @@ import java.util.List;
 @SpringBootTest
 class LottreyApplicationTests {
 
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    @Test
+    void test1() {
+//        String token = jwtHelper.createToken(123L);
+//        System.out.println(token);
+
+        System.out.println(jwtHelper.getUserId("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsInppcCI6IkdaSVAifQ" +
+                ".H4sIAAAAAAAA_6tWKi5NUrJSqizJLlbSUUqtKFCyMjQ3MrA0NjU2N9BRKi1OLfJMAYoZGdcCAL5FOucsAAAA" +
+                ".2RgLhcXgIhxJtg5RVZm3yDeOX7pZWjH4pKsM_Ztnh5c"));
+    }
+
+
     @Test
     void contextLoads() {
-        Integer[] i = {1231,21321,21,321,321,3,1};
+        Integer[] i = {1231, 21321, 21, 321, 321, 3, 1};
         ArrayList<Integer> list = new ArrayList<>();
         for (Integer integer : i) {
             list.add(integer);
@@ -24,9 +40,9 @@ class LottreyApplicationTests {
 
 
         Iterator<Integer> iterator = list.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             Integer next = iterator.next();
-            if (next == 21321){
+            if (next == 21321) {
                 iterator.remove();
             }
         }
